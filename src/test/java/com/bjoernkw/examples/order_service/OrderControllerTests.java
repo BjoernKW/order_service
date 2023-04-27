0b9158e970ce8a7eb97e0cb76b3cde864b72cd09
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-04-27 13:49:59 +0200

    First attempt at having initial order microservice code entirely by GitHub Copilot

---
+++ src/test/java/com/bjoernkw/examples/order_service/OrderControllerTests.java
@@ -0,0 +1,26 @@
+package com.bjoernkw.examples.order_service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class OrderControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+    
+    public void testGetAllOrders() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/order"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(3)));
+    }
+}
