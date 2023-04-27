248b90f3fd897daac93bb84f0b28385d09a50a5a
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-04-27 14:43:46 +0200

    First attempt at having initial order microservice code entirely by GitHub Copilot

--- src/main/java/com/bjoernkw/examples/order_service/OrderServiceApplication.java
+++ src/main/java/com/bjoernkw/examples/order_service/OrderServiceApplication.java
@@ -3,11 +3,6 @@ package com.bjoernkw.examples.order_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The main class of the order service.
- *
- * This microservice is responsible for managing orders.
- */
 @SpringBootApplication
 public class OrderServiceApplication {
 
